5297c309718bfe58196c2fe0d565b3602b2465da
wahyu <komang01966@gmail.com>
2023-04-08 16:44:32 +0800

    Add shoe table

---
+++ prisma/migrations/20230408075338_add_model_for_shoe/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Shoe" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "available" BOOLEAN NOT NULL
+);
