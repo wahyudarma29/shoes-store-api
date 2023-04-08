31cf51c5a18e857c0a4fade82274bf928b948c96
wahyu <komang01966@gmail.com>
2023-04-08 16:45:09 +0800

    Update index router and index js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const route = require("./routers");
 
 const app = express();
 
@@ -9,9 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello Express" });
-});
+app.use(route);
 
 app.listen(3000, () => {
   console.log("Running on localhost:3000");
